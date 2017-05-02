d237427b0724319064e6a1f0f1052b44f9f23c58
asterlan <asterlan2@gmail.com>
2017-05-02 23:46:07 +0200

    Add files via upload

---
+++ Arabot.py
@@ -0,0 +1,16 @@
+from telegram.ext import Updater, CommandHandler
+
+def start(bot, update):
+    update.message.reply_text('Hello World!')
+
+def hello(bot, update):
+    update.message.reply_text(
+        'Hello {}'.format(update.message.from_user.first_name))
+
+updater = Updater('312096173:AAG_5y7rbrg0G8XrQ6KGX3ZYbN5nGJRFk8E')
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+updater.start_polling()
+updater.idle()
