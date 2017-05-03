64d8f35f820c73869550fc47f43a87654b0df1d6
asterlan <asterlan2@gmail.com>
2017-05-03 20:29:18 +0200

    Add files via upload

--- Arabot.py
+++ Arabot.py
@@ -1,12 +1,12 @@
 from telegram.ext import Updater, CommandHandler
 
-TOKEN='312096173:AAG_5y7rbrg0G8XrQ6KGX3ZYbN5nGJRFk8E'
-updater = Updater(TOKEN)
+TOKEN='312096173:AAHB6GI8dm38d_NQnvsvHT1RlnqDWMensIk'
+#updater = Updater(TOKEN)
 
-updater.start_webhook(listen="0.0.0.0",
-                    port=8443,
-                    url_path=TOKEN)
-updater.bot.setWebhook("https://aragon1.herokuapp.com/" + TOKEN)
+#updater.start_webhook(listen="0.0.0.0",
+                    #port=8443,
+                    #url_path=TOKEN)
+#updater.bot.setWebhook("https://aragon1.herokuapp.com/" + TOKEN)
 #updater.idle()
 
 def start(bot, update):
